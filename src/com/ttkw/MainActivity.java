70137acb5e8e82e519bcf9ce925bef083542cdea
wph <840957885@qq.com>
2014-06-17 21:40:28 +0800

    TTKW项目成立

---
+++ src/com/ttkw/MainActivity.java
@@ -0,0 +1,22 @@
+package com.ttkw;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.mainactivity);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.mainactivity, menu);
+		return true;
+	}
+
+}
